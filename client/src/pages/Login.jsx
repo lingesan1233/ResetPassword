0aa7af09d8e448e06c9ad4bdc6c4010780f0d00d
lingesan <sglingesan@gmail.com>
2025-06-11 23:51:44 +0530

    color change

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
   };
 
   return (
-    <div>
+  <div className="page">
       <h2>Login</h2>
       <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
       <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -16,7 +16,7 @@ export default function Signup() {
   };
 
   return (
-    <div>
+    <div className="page">
       <h2>Signup</h2>
       <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
       <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
