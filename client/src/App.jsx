e5bc4b75f9f580963697717d29b9f4ea993a5ffa
lingesan <sglingesan@gmail.com>
2025-06-12 00:12:04 +0530

    app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import ResetPassword from './pages/ResetPassword';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
