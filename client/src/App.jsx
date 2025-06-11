ca72f315d09fb8d07c7eb742a7ca566a0ada600a
lingesan <sglingesan@gmail.com>
2025-06-12 00:18:59 +0530

    a

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import ResetPassword from './pages/ResetPassword';
-import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Login />} /> {/* Default page */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
-export default App; 
 
+export default App;
